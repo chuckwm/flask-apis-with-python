4120eb4edd03129c229787c0ffdaf7425dadbe80
Chuck MacKinnon <chuckwm@gmail.com>
2017-11-04 11:55:36 -0400

    First working files in section 2

---
+++ section2/variables_methods.py
@@ -0,0 +1,12 @@
+a = 5
+b = 10
+my_variable = 56
+# names can contain letters "_" or decimal numbers
+# must not start with a numbers
+string_variable = "hello"
+single_quotes = 'strings can have single quotes'
+
+print(my_variable)
+print(string_variable)
+print("hello, world!")
+print(123)

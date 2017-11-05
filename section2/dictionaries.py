4fc2114c3301173e3181141e52eacccd9532d69c
Chuck MacKinnon <chuckwm@gmail.com>
2017-11-05 10:44:31 -0500

    Adding dictionaries

---
+++ section2/dictionaries.py
@@ -0,0 +1,22 @@
+my_dict = {'name': 'Jose', 'age': 90, 'grades': [13, 45, 66, 90] }
+another_dict = { 1: 15, 2: 75, 3: 150 }
+
+lotter_player = {
+    'name': 'Rolf',
+    'numbers': {13, 45, 66, 23, 22}
+}
+
+universities = [
+    {
+        'name': 'Oxford',
+        'location': 'UK'
+    },
+    {
+        'name': 'MIT',
+        'location': 'US'
+    }
+]
+
+print(sum(lottery_player['numbers']))
+
+lottery_player['name'] = 'John'
